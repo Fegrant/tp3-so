1c237cbfc22d244dc7082643c98fba68cdd3749c
marc-holste <mholste@itba.edu.ar>
2021-11-12 18:58:58 -0300

    Added client to Makefile

---
+++ src/client.c
@@ -0,0 +1,25 @@
+#include "include/client.h"
+
+int main(int argc, char *argv[]) {
+    /*if(argc != 2) {
+        fprintf(stderr, "Error 2 arguments expected\n");
+        return -1;
+    }
+    if(strcmp(argv[1],SERVER_ADDR) != 0 || strcmp(argv[2],SERVER_PORT) != 0) {
+        fprintf(stderr, "Error wrong arguments\n");
+        return -1;
+    }
+    SockStruct server;
+    SockStruct client;
+    
+    
+    if((server.fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) == -1){
+        fprintf(stderr, "Error opening socket\n");
+        return -1;
+    }
+    
+    */
+    return 0;
+}
+
+//socket(AF_INET, SOCK_STREAM|SOCK_NONBLOCK, IPPROTO_TCP) = 3
